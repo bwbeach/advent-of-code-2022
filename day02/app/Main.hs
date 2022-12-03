84ebb854385fac8d8de17079875a6458d4e97c96
Brian Beach <coder@beachfamily.net>
2022-12-03 08:42:32 -0500

    second half of day 2

--- day02/app/Main.hs
+++ day02/app/Main.hs
@@ -11,17 +11,37 @@ runInput :: String -> IO ()
 runInput fileName = do
   input <- readFile fileName
   putStrLn fileName
-  print . day02 $ input
+  print . day02 chooser1 $ input
+  print . day02 chooser2 $ input
 
-day02 :: String -> Int
-day02 = sum . map scoreLine . endBy "\n"
+day02 :: (String -> Char -> String) -> String -> Int
+day02 chooser = sum . map (scoreLine chooser) . endBy "\n"
 
-scoreLine :: String -> Int
-scoreLine [theirCode, ' ', myCode] =
+chooser1 :: String -> Char -> String
+chooser1 _ = decode
+
+chooser2 :: String -> Char -> String
+chooser2 theirChoice myCode =
+  let outcome = decodeOutcome myCode
+   in if scoreOutcome "rock" theirChoice == outcome
+        then "rock"
+        else
+          if scoreOutcome "paper" theirChoice == outcome
+            then "paper"
+            else "scissors"
+
+decodeOutcome :: Char -> Int
+decodeOutcome 'X' = 0
+decodeOutcome 'Y' = 3
+decodeOutcome 'Z' = 6
+decodeOutcome _ = -1
+
+scoreLine :: (String -> Char -> String) -> String -> Int
+scoreLine chooser [theirCode, ' ', myCode] =
   let theirChoice = decode theirCode
-      myChoice = decode myCode
+      myChoice = chooser theirChoice myCode
    in scoreMyChoice myChoice + scoreOutcome myChoice theirChoice
-scoreLine _ = -100000000
+scoreLine _ _ = -100000000
 
 scoreOutcome :: String -> String -> Int
 scoreOutcome myChoice theirChoice
