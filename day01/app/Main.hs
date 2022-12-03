dbdc4cad19f415192ee2784101b025fc1de8ead1
Brian Beach <coder@beachfamily.net>
2022-12-02 21:11:59 -0500

    Day 1 working, but ugly

---
+++ day01/app/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+main :: IO ()
+main = do
+  runInput "test.txt"
+  runInput "input.txt"
+
+runInput :: String -> IO ()
+runInput fileName = do
+  input <- readFile fileName
+  putStrLn fileName
+  putStrLn . show . day01 1 $ input
+  putStrLn . show . day01 3 $ input
+
+day01 :: Int -> String -> Int
+day01 n input = sum . take n . reverse . sort $ map sumStrings (splitOn [""] (endBy "\n" input))
+
+readInt :: String -> Int
+readInt s = read s :: Int
+
+sumStrings :: [String] -> Int
+sumStrings list = sum (map readInt list)
