8ab7d530ad8c483c5c4e21c3c47438225f09482d
Brian Beach <coder@beachfamily.net>
2022-12-16 14:21:21 -1000

    day 11, part 2

--- day11/app/Main.hs
+++ day11/app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import qualified Data.IntMap.Strict as IntMap
 import Data.List (sort)
 import Data.List.Split (splitOn)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
@@ -15,11 +16,31 @@ runInput :: String -> IO ()
 runInput fileName = do
   input <- readFile fileName
   putStrLn fileName
-  print . day11 20 $ input
+  print . day11 20 part1WorryReducerFactory $ input
+  print . day11 10000 part2WorryReducerFactory $ input
 
-day11 :: Int -> String -> Int
-day11 numRounds =
-  product . take 2 . reverse . sort . map inspections . IntMap.elems . runNRounds numRounds . parseInput
+day11 :: Int -> (Flock -> (Int -> Int)) -> String -> Int
+day11 numRounds worryReducerFactory =
+  product
+    . take 2
+    . reverse
+    . sort
+    . traceIt
+    . map inspections
+    . IntMap.elems
+    . runNRounds numRounds
+    . (\f -> IntMap.map (setWorryReducer (worryReducerFactory f)) f)
+    . parseInput
+
+traceIt :: Show a => a -> a
+traceIt a = trace ("AAA " ++ show a) a
+
+part1WorryReducerFactory :: Flock -> (Int -> Int)
+part1WorryReducerFactory _ = (`div` 3)
+
+part2WorryReducerFactory :: Flock -> (Int -> Int)
+part2WorryReducerFactory flock n =
+  n `rem` product (map testDivisibleBy (IntMap.elems flock))
 
 type Flock = IntMap.IntMap Monkey
 
@@ -27,11 +48,15 @@ data Monkey = Monkey
   { number :: Int,
     items :: [Int],
     operation :: Int -> Int,
-    test :: Int -> Bool,
+    worryReducer :: Int -> Int,
+    testDivisibleBy :: Int,
     ifTrue :: Int,
     ifFalse :: Int,
     inspections :: Int
   }
+
+setWorryReducer :: (Int -> Int) -> Monkey -> Monkey
+setWorryReducer wr m = m {worryReducer = wr}
 
 runNRounds :: Int -> Flock -> Flock
 runNRounds n flock = iterate runRound flock !! n
@@ -49,8 +74,8 @@ runMonkey flock0 n =
 runItem :: Monkey -> Flock -> Int -> Flock
 runItem monkey flock level0 =
   let level1 = operation monkey level0
-      level2 = level1 `div` 3
-      target = if test monkey level2 then ifTrue monkey else ifFalse monkey
+      level2 = worryReducer monkey level1
+      target = if level2 `rem` testDivisibleBy monkey == 0 then ifTrue monkey else ifFalse monkey
       targetMonkey0 = flock IntMap.! target
       targetMonkey1 = targetMonkey0 {items = items targetMonkey0 ++ [level2]}
    in IntMap.insert target targetMonkey1 flock
@@ -67,7 +92,8 @@ parseMonkey [line0, line1, line2, line3, line4, line5] =
     { number = read . last . words $ line0,
       items = map read . drop 2 . words $ line1,
       operation = parseOperation . words $ line2,
-      test = parseTest . words $ line3,
+      worryReducer = id, -- will be updated
+      testDivisibleBy = parseTestDivisibleBy . words $ line3,
       ifTrue = read . last . words $ line4,
       ifFalse = read . last . words $ line5,
       inspections = 0
@@ -83,8 +109,8 @@ parseOp :: String -> Int -> (Int -> Int)
 parseOp "*" n x = x * n
 parseOp "+" n x = x + n
 
-parseTest :: [String] -> (Int -> Bool)
-parseTest ["Test", "divisible", "by", s] n = n `rem` read s == 0
+parseTestDivisibleBy :: [String] -> Int
+parseTestDivisibleBy ["Test", "divisible", "by", s] = read s
 
 removeCommasAndColons :: String -> String
 removeCommasAndColons = filter (\c -> c /= ',' && c /= ':')
