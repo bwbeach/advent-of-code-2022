efec96403253dd6f77203a9f2b81dc4d81fb756a
Brian Beach <coder@beachfamily.net>
2022-12-25 15:26:11 -1000

    my first HUnit test

--- day16/src/MyLib.hs
+++ day16/src/MyLib.hs
@@ -1,4 +1,7 @@
-module MyLib (someFunc) where
+module MyLib (someFunc, add1) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+add1 :: Int -> Int
+add1 = (+ 1)
--- day16/test/Main.hs
+++ day16/test/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+import MyLib (add1)
+import qualified System.Exit as Exit
+import Test.HUnit (Test (..), assertEqual, failures, runTestTT)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  result <- runTestTT tests
+  if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
+
+test1 :: Test
+test1 = TestCase (assertEqual "should return 3" 3 (add1 2))
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1]
