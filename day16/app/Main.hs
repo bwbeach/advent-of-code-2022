ddc26a6ef64730e5d536bfc83712944b5ddfe9a0
Brian Beach <coder@beachfamily.net>
2022-12-27 07:33:10 -1000

    checkpoint

--- day16/app/Main.hs
+++ day16/app/Main.hs
@@ -1,5 +1,112 @@
 module Main where
 
+import Data.Graph.DGraph
+import qualified Data.Graph.DGraph as DG
+import Data.Graph.Types
+import Data.Hashable (Hashable)
+import Data.List (sort)
+import qualified Data.Map.Strict as M (Map, fromList, member)
+import Data.Maybe (mapMaybe)
+import Debug.Trace
+import Dijkstra (distances)
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  runInput "test.txt"
+
+-- runInput "input.txt"
+
+runInput :: String -> IO ()
+runInput fileName = do
+  input <- readFile fileName
+  putStrLn fileName
+  print . day16 $ input
+
+day16 :: String -> DGraph String Int
+day16 text =
+  let -- read the non-zero valves and their rates
+      valves = parseValves text
+      -- read the graph as presented
+      g0 = traceGraph "AAA" (parseGraph text)
+      -- compute the distances between all pairs of nodes
+      g1 = traceGraph "BBB" $ distances g0
+      -- filter the graph down to just the nodes with valves
+      g2 = traceGraph "CCC" $ filterGraph (`M.member` valves) g1
+   in g2
+
+traceIt :: Show a => [Char] -> a -> a
+traceIt lbl a = trace (lbl ++ " " ++ show a) a
+
+traceGraph :: (Hashable v, Ord v, Show v, Show e) => String -> DGraph v e -> DGraph v e
+traceGraph lbl g = trace (lbl ++ "\n" ++ graphToGrid g) g
+
+-- Returns a subgraph containing only the listed nodes
+filterGraph :: (String -> Bool) -> DGraph String Int -> DGraph String Int
+filterGraph p = fromArcsList . filter (\(Arc a b _) -> p a || p b) . toArcsList
+
+-- Returns a map from valve name to flow rate for all non-zero valves.
+-- We never turn on the valves that have 0, so they don't matter.
+parseValves :: String -> M.Map String Int
+parseValves = M.fromList . filter (\(_, n) -> n /= 0) . map parseValve . lines
+
+parseValve :: String -> (String, Int)
+parseValve s0 =
+  let s1 = map (replaceChar '=' ' ' . replaceChar ';' ' ') s0
+      ws = words s1
+   in (ws !! 1, read (ws !! 5))
+
+parseGraph :: String -> DGraph String Int
+parseGraph = fromArcsList . traceIt "ARCS" . concatMap parseArc . lines
+
+parseArc :: String -> [Arc String Int]
+parseArc s0 =
+  let ws = words . map (replaceChar ',' ' ') $ s0
+      src = ws !! 1
+      dests = traceIt "DESTS" $ drop 9 ws
+   in map (\dest -> Arc src dest 1) dests
+
+replaceChar :: Char -> Char -> Char -> Char
+replaceChar a b c = if c == a then b else c
+
+graphToGrid :: (Hashable v, Ord v, Show v, Show e) => DGraph v e -> String
+graphToGrid g =
+  let vs = sort . vertices $ g
+      header = "" : map show vs
+      dataRows = map (makeDataRow g vs) vs
+      rows = header : dataRows
+   in formatGrid rows
+
+makeDataRow :: (Hashable v, Show v, Show e) => DGraph v e -> [v] -> v -> [String]
+makeDataRow g vs v =
+  show v : map (maybe "" show . graphLookup g v) vs
+
+graphLookup :: (Eq v, Hashable v) => DGraph v e -> v -> v -> Maybe e
+graphLookup g v w =
+  listToMaybe . mapMaybe (checkArc v w) $ DG.outboundingArcs g v
+
+checkArc :: (Eq v) => v -> v -> Arc v e -> Maybe e
+checkArc v w (Arc v' w' e) =
+  if v == v' && w == w'
+    then Just e
+    else Nothing
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe [a] = Just a
+listToMaybe _ = error "more than one item in list"
+
+showMaybe :: Show a => Maybe a -> String
+showMaybe = maybe "" show
+
+formatGrid :: [[String]] -> String
+formatGrid rows =
+  let cellWidth = maximum . map length . concat $ rows
+   in unlines . map (formatLine cellWidth) $ rows
+
+formatLine :: Int -> [String] -> String
+formatLine cellWidth = unwords . map (leftPad cellWidth)
+
+leftPad :: Int -> String -> String
+leftPad n s =
+  let needed = n - length s
+   in replicate needed ' ' ++ s
