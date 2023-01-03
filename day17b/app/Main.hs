964ae4ce78b6b946d26e09be91a56d74f4af21d4
Brian Beach <coder@beachfamily.net>
2023-01-03 08:55:08 -1000

    check for repeats

--- day17b/app/Main.hs
+++ day17b/app/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Control.Monad.State
+import qualified Data.Map.Strict as M
 import Data.Maybe
 import Debug.Trace
 import MyLib
@@ -10,6 +11,7 @@ import MyLib
 main :: IO ()
 main = do
   runWithFile "test.txt"
+  runWithFile "input.txt"
 
 runWithFile :: String -> IO ()
 runWithFile fileName = do
@@ -18,33 +20,85 @@ runWithFile fileName = do
   let rocks = parseRocks rockText
   inputText <- readFile fileName
   let jets = parseJets inputText
-  print rocks
-  print jets
   let rjc = execState (dropNRocks 2022) (initialRJC rocks jets)
-  putStrLn (unlines (rjcCave rjc))
-  print (length (rjcCave rjc) - 1)
+  -- putStrLn (unlines (rjcCave rjc))
+  print (rjcCheckpoints rjc)
+  print (rjcRepeatHeight rjc + length (rjcCave rjc) - 1)
 
 initialRJC :: [Rock] -> [Jet] -> RJC
 initialRJC rocks jets =
   RJC
-    { rjcRocks = cycle rocks,
-      rjcJets = cycle jets,
-      rjcCave = ["#######"]
+    { rjcRocks = cycle (zip [0 ..] rocks),
+      rjcJets = cycle (zip [0 ..] jets),
+      rjcCave = ["#######"],
+      rjcPrevJetNumber = negate 1, -- TODO - use a Maybe
+      rjcCheckpoints = M.empty,
+      rjcRepeatHeight = 0
     }
 
 data RJC = RJC
-  { rjcRocks :: [Rock],
-    rjcJets :: [Jet],
-    rjcCave :: Cave
+  { rjcRocks :: [(Int, Rock)],
+    rjcJets :: [(Int, Jet)],
+    rjcCave :: Cave,
+    -- the number of the first jet on the previous rock
+    rjcPrevJetNumber :: Int,
+    -- map from (rockNumber, jetNumber, topNOfCave) to (numberRemaining, cave height)
+    rjcCheckpoints :: M.Map (Int, Int, Cave) (Int, Int),
+    -- height from repeats, not included in actual cave because we didn't actually do them
+    rjcRepeatHeight :: Int
   }
 
+rjcPeekJetNumber :: RJC -> Int
+rjcPeekJetNumber = fst . head . rjcJets
+
+rjcPeekRockNumber :: RJC -> Int
+rjcPeekRockNumber = fst . head . rjcRocks
+
 dropNRocks :: Int -> State RJC ()
-dropNRocks n =
-  if n == 0
+dropNRocks n = do
+  n' <- checkForRepeat n
+  if n' == 0
     then return ()
     else do
       dropRock
-      dropNRocks (n - 1)
+      dropNRocks (n' - 1)
+
+checkForRepeat :: Int -> State RJC Int
+checkForRepeat n = do
+  rjc <- get
+  let jn = rjcPeekJetNumber rjc
+  if jn < rjcPrevJetNumber rjc
+    then do
+      let c = rjcCave rjc
+      let rn = rjcPeekRockNumber rjc
+      let top = take 40 c
+      let h = length c
+      let k = (rn, jn, top)
+      let cp = rjcCheckpoints rjc
+      case M.lookup k cp of
+        Nothing -> do
+          put
+            ( rjc
+                { rjcCheckpoints = M.insert (rn, jn, top) (n, h) (rjcCheckpoints rjc),
+                  rjcPrevJetNumber = jn
+                }
+            )
+          return n
+        Just (prevN, prevHeight) -> do
+          let deltaH = traceIt "deltaH" $ h - prevHeight
+          let deltaN = traceIt "deltaN" $ prevN - n
+          let nRepeats = traceIt "nRepeats" $ n `div` deltaN
+          put
+            ( rjc
+                { rjcCheckpoints = M.empty,
+                  rjcPrevJetNumber = jn,
+                  rjcRepeatHeight = rjcRepeatHeight rjc + nRepeats * deltaH
+                }
+            )
+          return (traceIt ("NNN " ++ show n ++ " ") (n - nRepeats * deltaN))
+    else do
+      put rjc {rjcPrevJetNumber = jn}
+      return n
 
 dropRock :: State RJC ()
 dropRock = do
@@ -60,10 +114,10 @@ dropRockAt r y = do
   jet <- nextJet
   let r' = applyJet jet r y c
   case applyGravity r' y c of
-    Just y' -> dropRockAt (traceRock "DRA-r" r') y'
+    Just y' -> dropRockAt r' y'
     Nothing -> do
       rjc' <- get
-      let c' = fromJust $ addRock (traceRock "DRA-R" r') y c
+      let c' = fromJust $ addRock r' y c
       put rjc' {rjcCave = c'}
       return ()
 
@@ -89,17 +143,34 @@ applyJet jet r y c =
       JetLeft -> moveRockLeft r
       JetRight -> moveRockRight r
 
+getCave :: State RJC Cave
+getCave = do
+  rjc <- get
+  return (rjcCave rjc)
+
+peekJetNumber :: State RJC Int
+peekJetNumber = do
+  s <- get
+  let ((n, _) : _) = rjcJets s
+  return n
+
 nextJet :: State RJC Jet
 nextJet = do
   s <- get
-  let (j : js) = rjcJets s
+  let ((_, j) : js) = rjcJets s
   put (s {rjcJets = js})
   return j
+
+peekRockNumber :: State RJC Int
+peekRockNumber = do
+  s <- get
+  let ((n, _) : _) = rjcRocks s
+  return n
 
 nextRock :: State RJC Rock
 nextRock = do
   s <- get
-  let (r : rs) = rjcRocks s
+  let ((_, r) : rs) = rjcRocks s
   put (s {rjcRocks = rs})
   return r
 
