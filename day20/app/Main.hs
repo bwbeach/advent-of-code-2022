615486829712a38735060272d308055857fe534c
Brian Beach <coder@beachfamily.net>
2023-01-26 14:16:27 -1000

    day 20, part 1

--- day20/app/Main.hs
+++ day20/app/Main.hs
@@ -1,8 +1,51 @@
-module Main where
+module Main (main) where
 
-import qualified MyLib (someFunc)
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  runWithFile "test.txt"
+  runWithFile "input.txt"
+
+runWithFile :: FilePath -> IO ()
+runWithFile fileName = do
+  input <- readFile fileName
+  putStrLn fileName
+  let numbers = map read . lines $ input
+  print . day20 $ numbers
+
+day20 :: [Int] -> Int
+day20 numbers = coords (mix numbers numbers)
+
+-- | Add the coordinates
+coords :: [Int] -> Int
+coords ns =
+  sum . map nthAfterZero $ [1000, 2000, 3000]
+  where
+    len = length ns
+    indexOfZero = fromJust (elemIndex 0 ns)
+    nthAfterZero i = ns !! ((i + indexOfZero) `mod` len)
+
+-- | Perform a full mixing
+mix :: [Int] -> [Int] -> [Int]
+mix spec state =
+  foldl (moveOne len) state spec
+  where
+    len = length spec
+
+-- | Moves one number to a new place.
+moveOne :: Int -> [Int] -> Int -> [Int]
+moveOne len state n =
+  before ++ [n] ++ after
+  where
+    -- we always move forward.  with a negative number, we wrap the
+    -- circular list and move it forward.  The moving forward happens
+    -- in a list that does not have n it, so the modulus is (len - 1).
+    delta = n `mod` (len - 1)
+    -- make a list, starting with the number after n, that is at least long enough
+    afterN = drop 1 . dropWhile (/= n) $ (state ++ state)
+    -- break the list at the point where n will be inserted
+    (before, later) = splitAt delta afterN
+    -- the 'later' part may be too long, so trim it down
+    after = take (len - delta - 1) later
