9b65708c59878326c69309254290e1a6fca24ae0
Brian Beach <coder@beachfamily.net>
2023-01-29 07:56:49 -1000

    day 22, part 1

--- day22/app/Main.hs
+++ day22/app/Main.hs
@@ -1,8 +1,121 @@
-module Main where
+module Main (main) where
 
-import qualified MyLib (someFunc)
+import Data.Char
+import Data.List.Split
+import Data.Maybe
+import Debug.Trace
+import Linear.V2 (V2 (..))
+import MyLib
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  runWithFile "test.txt"
+  runWithFile "input.txt"
+
+runWithFile :: String -> IO ()
+runWithFile fileName = do
+  putStrLn fileName
+  text <- readFile fileName
+  let (grid, instructions) = parseInput text
+  let result = day22a grid instructions
+  let (resultGrid, _, _) = result
+  putStrLn "\n"
+  putStrLn . unlines . gridToStrings $ resultGrid
+  print . day22aCode $ result
+
+day22a :: Grid -> [Instruction] -> State
+day22a grid =
+  foldl applyInstruction startState
+  where
+    startState = (grid, startingPoint grid, V2 1 0)
+
+day22aCode :: State -> Int
+day22aCode (_, V2 x y, d) = y * 1000 + 4 * x + dirCode d
+
+dirCode :: V2 Int -> Int
+dirCode (V2 1 0) = 0
+dirCode (V2 (-1) 0) = 2
+dirCode (V2 0 (-1)) = 3
+dirCode (V2 0 1) = 1
+dirCode v = error ("bad direction: " ++ show v)
+
+parseInput :: String -> (Grid, [Instruction])
+parseInput text =
+  case splitOn [""] . lines $ text of
+    [gridText, instructionsText] -> (gridFromStrings (V2 1 1) gridText, parseInstructions (concat instructionsText))
+    _ -> error ("bad input: " ++ text)
+
+data Instruction = TurnLeft | TurnRight | Move Int deriving (Eq, Show)
+
+parseInstructions :: String -> [Instruction]
+parseInstructions [] = []
+parseInstructions (c : cs)
+  | isDigit c = parseMove (c : cs)
+  | c == 'L' = TurnLeft : parseInstructions cs
+  | c == 'R' = TurnRight : parseInstructions cs
+  | c == '\n' = parseInstructions cs
+  | otherwise = error ("bad instructions: " ++ (c : cs))
+
+parseMove :: [Char] -> [Instruction]
+parseMove s =
+  Move (read ns) : parseInstructions rest
+  where
+    (ns, rest) = span isDigit s
+
+-- | Find the initial position: the first '.' on the top row
+startingPoint :: Grid -> V2 Int
+startingPoint g =
+  head . filter isDot . iterate (+ V2 1 0) $ gridTopLeft g
+  where
+    isDot p = gridLookup p g == Just '.'
+
+-- | As we're moving around, our state is the grid, plus our position and direction.
+type State = (Grid, V2 Int, V2 Int)
+
+applyInstruction :: State -> Instruction -> State
+applyInstruction s@(grid, pos0, dir0) instr =
+  case instr of
+    TurnLeft -> (grid, pos0, turnLeft dir0)
+    TurnRight -> (grid, pos0, turnRight dir0)
+    Move n -> move s n
+
+turnLeft :: V2 Int -> V2 Int
+turnLeft (V2 x y) = V2 y (negate x)
+
+turnRight :: V2 Int -> V2 Int
+turnRight (V2 x y) = V2 (negate y) x
+
+turnAround :: V2 Int -> V2 Int
+turnAround (V2 x y) = V2 (negate x) (negate y)
+
+-- | Move n positions form where we are in the direction we're facing, or until hitting a wall.
+move :: State -> Int -> State
+move s 0 = s
+move s@(grid, pos, dir) n =
+  case look nextPos of
+    Nothing -> error "should have wrapped"
+    Just '#' -> s
+    Just _ -> move (markedGrid, nextPos, dir) (n - 1)
+  where
+    candidatePos = pos + dir
+    needToWrap = isEmpty candidatePos
+    nextPos = if needToWrap then wrap pos dir else candidatePos
+    markedGrid = gridInsert pos (dirChar dir) grid
+    isEmpty p = isNothing $ look p
+    look p = gridLookup p grid
+    -- wrap around from a spot that is non-empty.  the problem description says
+    -- to "look in the opposite direction" until finding the edge of the board
+    wrap p d =
+      if isEmpty p' then p else wrap p' d
+      where
+        p' = p + turnAround d
+
+dirChar :: V2 Int -> Char
+dirChar (V2 1 0) = '>'
+dirChar (V2 (-1) 0) = '<'
+dirChar (V2 0 (-1)) = '^'
+dirChar (V2 0 1) = 'v'
+dirChar v = error ("bad direction: " ++ show v)
+
+traceIt :: Show a => [Char] -> a -> a
+traceIt lbl x = trace (lbl ++ " " ++ show x) x
--- day22/src/MyLib.hs
+++ day22/src/MyLib.hs
@@ -1,4 +1,102 @@
-module MyLib (someFunc) where
+module MyLib
+  ( Grid,
+    gridFromList,
+    gridFromStrings,
+    gridToStrings,
+    gridLookup,
+    gridInsert,
+    gridTopLeft,
+    pointX,
+    pointY,
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Linear.V2 (V2 (..))
+
+-- | A possibly sparse grid of characters.
+--
+-- Cells are named with (V2 Int) vectors containing x and y coordinates.
+-- Right is positive x, down is positive y.
+--
+-- When printing and showing, a cell with no value is represented as a space.
+data Grid = Grid
+  { gridMinX :: Int,
+    gridMaxX :: Int,
+    gridMinY :: Int,
+    gridMaxY :: Int,
+    gridCells :: M.Map (V2 Int) Char
+  }
+
+instance Show Grid where
+  showsPrec d g =
+    showParen (d > prec) $
+      showString "gridFromStrings "
+        . showsPrec (prec + 1) (V2 (gridMinX g) (gridMinY g))
+        . showString " "
+        . showsPrec (prec + 1) (gridToStrings g)
+    where
+      prec = 10
+
+gridTopLeft :: Grid -> V2 Int
+gridTopLeft g = V2 (gridMinX g) (gridMinY g)
+
+gridToStrings :: Grid -> [String]
+gridToStrings g =
+  map gridLine [gridMinY g .. gridMaxY g]
+  where
+    gridLine y = map (gridCell y) [gridMinX g .. gridMaxX g]
+    gridCell :: Int -> Int -> Char
+    gridCell y x = fromMaybe ' ' . M.lookup (V2 x y) . gridCells $ g
+
+-- | Creates a grid from a list of (pos, value) pairs.
+gridFromList :: [(V2 Int, Char)] -> Grid
+gridFromList items =
+  Grid
+    { gridMaxX = maximum . map pointX $ points,
+      gridMaxY = maximum . map pointY $ points,
+      gridMinX = minimum . map pointX $ points,
+      gridMinY = minimum . map pointY $ points,
+      gridCells = M.fromList items
+    }
+  where
+    points = map fst items
+
+-- | Creates a grid of characters by parsing a grid of characters in strings.
+gridFromStrings :: V2 Int -> [String] -> Grid
+gridFromStrings (V2 minX minY) strs =
+  Grid
+    { gridMaxX = minX + maximum (map length strs) - 1,
+      gridMaxY = minY + length strs - 1,
+      gridMinX = minX,
+      gridMinY = minY,
+      gridCells = M.fromList items
+    }
+  where
+    items = concat $ zipWith lineItems [minY ..] strs
+    lineItems y str = catMaybes $ zipWith (oneItem y) [minX ..] str
+    oneItem y x c
+      | c == ' ' = Nothing
+      | otherwise = Just (V2 x y, c)
+
+pointX :: V2 Int -> Int
+pointX (V2 x _) = x
+
+pointY :: V2 Int -> Int
+pointY (V2 _ y) = y
+
+-- | Returns the character at the given position
+gridLookup :: V2 Int -> Grid -> Maybe Char
+gridLookup p g = M.lookup p (gridCells g)
+
+-- | Sets the character at the given position
+gridInsert :: V2 Int -> Char -> Grid -> Grid
+gridInsert p@(V2 x y) c g =
+  g
+    { gridMaxX = max x (gridMaxX g),
+      gridMaxY = max y (gridMaxY g),
+      gridMinX = min x (gridMinX g),
+      gridMinY = min y (gridMinY g),
+      gridCells = M.insert p c (gridCells g)
+    }
